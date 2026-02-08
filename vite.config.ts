c23e9fa5077476bd40c01abf95b62e11155efb5f
d1fay-web <bstepin@internet.ru>
2026-02-09 02:40:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,8 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/my-portfolio/",
+  
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
